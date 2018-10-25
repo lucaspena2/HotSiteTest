c807e70fe44c4cb3eb8092ef9f4d0980b05f837f
lucaspena2@gmail.com <Lucasgnr 04>
2018-10-25 14:50:17 -0300

    Initial commit

---
+++ Includes/Header.php
@@ -0,0 +1,28 @@
+<!-- Header.php -->
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Class Crud</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+
+<body>
+<div class="Banner">
+    <img src="Images/Banner.png">
+</div>
+
+
+<div class="Nav">
+    <li><a href="DATA">DATA</a></li>
+    <li><a href="PROGRAMAS/DISCIPLINAS">PROGRAMAS/DISCIPLINAS</a></li>
+    <li><a href="PROFESSORES/Nº DE QUESTÕES">PROFESSORES/Nº DE QUESTÕES</a></li>
+    <li><a href="VIDEOS">VÍDEOS</a></li>
+</div>
+
+
+<!-- Footer.php -->
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php include("Includes/Header.php"); ?>
+<?php include("Includes/Footer.php"); ?>
